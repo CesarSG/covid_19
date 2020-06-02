ac41f63f582d0dcfa96c960a83f6cdd845bd080c
CesarSG <cesar_sevilla@outlook.com>
2020-06-01 19:13:40 -0500

    Up components

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,12 @@
+import React, {useState,useEffect} from 'react';
+
+const Chart = () => {
+
+    const [dailyData, setDailyDate] = useState({});
+
+    return(
+        <h1>Chart</h1>
+    );
+}
+
+export default Chart;
