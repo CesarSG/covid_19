0e966f179c709ec3ff929ddbd9c2f7134a4ca65d
CesarSG <cesar_sevilla@outlook.com>
2020-06-03 18:11:33 -0500

    Fetch country data

--- src/api/index.js
+++ src/api/index.js
@@ -3,14 +3,21 @@ import axios from 'axios';
 const url = 'https://covid19.mathdro.id/api';
 
 
-export const fetchedtData = async () => {
+export const fetchedtData = async (country) => {
+
+    let changeableURL = url;
+
+    if(country){
+        changeableURL = `${url}/countries/${country}`;
+    }
+
     try {
-        const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
+        const {data: {confirmed,recovered,deaths,lastUpdate}} = await axios.get(changeableURL);
 
         return { confirmed, recovered, deaths, lastUpdate};
         
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
@@ -29,6 +36,17 @@ export const fetchDailyData = async () => {
         return modifiedData;
 
     } catch (error) {
-        
+        console.log(error);
+    }
+}
+
+export const fetchCountries = async () => {
+    try {
+        const {data: {countries}} = await axios.get(`${url}/countries`);
+
+        return countries.map((country) => country.name);
+
+    } catch (error) {
+        console.log(error);
     }
 }
