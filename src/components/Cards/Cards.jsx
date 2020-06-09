85a8e2ae9f69d11978b664f722f726ffb6fcf708
CesarSG <cesar_sevilla@outlook.com>
2020-06-08 20:51:23 -0500

    Add Pct to Cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -8,11 +8,26 @@ import styles from './Cards.module.css';
 import alert from '../../images/alert.svg';
 import patient from '../../images/patient.svg';
 import bandage from '../../images/bandage.svg';
+import heart_rate from '../../images/heart-rate.svg';
 
-const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate}}) => {
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate, pctDeaths, pctRecovered}}) => {
+
 
     if(!confirmed){
         return "Loading..." ;
+    }
+
+    if(confirmed){
+
+        let getPct = (value,confirmed) => {
+            let pct = (value * 100)/confirmed;
+            pct = pct.toFixed(2);
+            
+            return pct;
+        }
+
+        let pctDeaths = getPct(deaths.value,confirmed.value);
+        let pctRecovered = getPct(recovered.value,confirmed.value);
     }
 
     return(
@@ -64,6 +79,29 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate}}) => {
                         </h5>
                         <p className={styles.date}>{new Date(lastUpdate).toDateString()}</p>
                         <p>Number of active cases of COVID-19</p>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card} xs={12} md={4} className={cx(styles.card, styles.cardColor, styles.cardPct)}>
+                    <CardContent>
+                        <img className={styles.iconCardPct} alt="covid-19" src={heart_rate}/>
+                        <p className={styles.cardTitle}>Recovered</p>
+                        <h5 className={styles.recovered}>
+                            <CountUp
+                                start={0}
+                                end={pctRecovered}
+                                duration={2.5}
+                                decimals={3}
+                            /> %
+                        </h5>
+                        <p className={styles.cardTitle}>Deaths</p>
+                        <h5 className={styles.deaths}>
+                            <CountUp
+                                start={0}
+                                end={pctDeaths}
+                                duration={2.5}
+                                decimals={4}
+                            /> %
+                        </h5>
                     </CardContent>
                 </Grid>
             </Grid>
